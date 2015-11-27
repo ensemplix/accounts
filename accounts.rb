39217be311f7d6bdea48bed486f517dc2719cac1
Vladislavs Golubs <ensirius@ensemplix.ru>
2015-11-27 21:34:20 +0200

    Добавлена валидация имен и паролей

--- accounts.rb
+++ accounts.rb
@@ -24,11 +24,22 @@ DataMapper.finalize
 
 before do
   content_type :json
+end
+
+before do
   param :token, String, required: true
 
   unless params[:token].eql? $TOKEN
     halt 401, {:message => 'Parameter is invalid', :errors => {:token => 'Parameter is invalid'}}.to_json
   end
+end
+
+before do
+  param :username, String, required: true, min_length: 3, max_length: 15
+end
+
+before /^(?!\/(lookup))/ do
+  param :password, String, required: true, min_length: 5, max_length: 25
 end
 
 post '/register' do
